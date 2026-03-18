ae620cc24af040869d4e1c686bfd57fc8fb0be58
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-03-18 03:22:51 -0400

    updated router

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,6 +1,7 @@
 import {ProductsList} from '@/Types'
 import Categories from './Categories'
 import ProductCard from './ProductCard'
+import Filter from './Filter'
 
 const products: ProductsList = [
   {
@@ -116,10 +117,11 @@ const products: ProductsList = [
 import React from 'react'
 import Link from 'next/link';
 
-const ProductList = ({category}:{category:string}) => {
+const ProductList = ({category, params}:{category:string, params: "homepage" | "products"}) => {
   return (
     <div className='w-full'>
       <Categories />
+      {params === 'products' && <Filter />}
       <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12'>
             {products.map(product => <ProductCard key = {product.id} product = {product}/>)}
       </div>
